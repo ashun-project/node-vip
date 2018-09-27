a45b69a98d1b4e86d7eba0638ec145ff279f01e2
ashun <ashun@ashundeMacBook-Pro.local>
2018-09-27 23:22:40 +0800

    x

--- index.js
+++ index.js
@@ -1,76 +1,123 @@
 var express = require('express')
 var app = express();
+var cookie = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var path = require('path');
 var api = require('./api');
-// var mysql = require('mysql');
-// var pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ashun666',
-//     database: 'vip'
-// });
-// router.post('/api/getAllList', function (req, res) {
-//     var sql = 'SELECT * FROM ' + req.body.title + 'list';
-//     pool.getConnection(function (err, conn) {
-//         if (err) console.log("POOL ==> " + err);
-//         conn.query(sql, function (err, result) {
-//             if (err) {
-//                 console.log('[SELECT ERROR] - ', err.message);
-//                 res.send('error');
-//             } else {
-//                 res.json(result);
-//             }
-//             conn.release();
-//         });
-//     })
-// })
-// router.post('/api/getList', function (req, res) {
-//     // console.log(html)
-//     var currentIp = getIp(req)
-//     console.log('getList----' + req.body.title + '=====', currentIp)
-//     if (currentIp.indexOf('195.201.218.75') > -1) {
-//         // console.log('getList----',currentIp)
-//         // res.send('who are you');
-//         // return;
-//     }
-//     var limit = ((req.body.current - 1) * 20) + ',' + 20;
-//     var sql = 'SELECT * FROM ' + req.body.title + 'list order by createTime desc limit ' + limit;
-//     var count = 'SELECT COUNT(*) FROM ' + req.body.title + 'list';
-//     pool.getConnection(function (err, conn) {
-//         if (err) console.log("POOL ==> " + err);
-//         conn.query(sql, function (err, result) {
-//             if (err) {
-//                 console.log('[SELECT ERROR] - ', err.message);
-//                 res.send('error');
-//                 conn.release();
-//             } else {
-//                 // res.json({total:21, data:result});
-//                 conn.query(count, function (err, num) {
-//                     if (err) {
-//                         res.send('error');
-//                     } else {
-//                         var arr = result.map(item => {
-//                             return {
-//                                 id: item.createTime,
-//                                 title: item.title,
-//                                 img: item.img
-//                             }
-//                         })
-//                         res.json({
-//                             total: num[0]['COUNT(*)'],
-//                             list: arr
-//                         });
-//                     }
-//                     conn.release();
-//                 })
-//             }
-//         });
-//     })
-// })
 
+
+app.use(cookie());
+app.use(session({
+    resave: true, // 是指每次请求都重新设置session cookie，假设你的cookie是6000毫秒过期，每次请求都会再设置6000毫秒
+    saveUninitialized: false, // 是指无论有没有session cookie，每次请求都设置个session cookie 
+    secret: '123456', //  加密
+    name: 'testapp', //这里的name值得是cookie的name，默认cookie的name是：connect.sid
+    cookie: {
+        maxAge: 18000000
+    }, //设置maxAge是80000ms，即80s后session和相应的cookie失效过期
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.set('view engine','jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(api);
 
 
 app.listen(1337);
+
+
+// function vaidParams(userName, password) {
+//     var error = '';
+//     if (!userName || !password) {
+//         error = '用户或密码不能为空';
+//     }
+//     if (userName.length > 12 || password.length > 12) {
+//         error = '用户或密码不可超过12位';
+//     }
+//     if (userName.length < 3 || password.length < 3) {
+//         error = '用户或密码不可小于3位';
+//     }
+//     return error;
+// }
+
+// // 接口
+// app.post('/vdidLogin', function (req, res, next) {
+//     if (req.session.loginUser) {
+//         res.json(req.session.loginUser);
+//     } else {
+//         res.json({error: 'no'});
+//     }
+// });
+
+
+
+// // 重新查询登录
+// app.post('/refreshLogin', function (req, res, next) {
+//     var user = req.session.loginUser;
+//     if (user && user.userName) {
+//         var sql = 'SELECT * FROM list where userName = "'+ user.userName + '"';
+//         pool.getConnection(function (err, conn) {
+//             if (err) console.log("POOL refresh-register==> " + err);
+//             conn.query(sql, function (err, result) {
+//                 if (result.length) {
+//                     delete result[0].password;
+//                     req.session.loginUser = result[0];
+//                 }
+//                 res.json({success: '结束'});
+//                 conn.release();
+//             });
+//         });
+//     } else {
+//         res.json({error: ''});
+//     }
+// });
+
+// // 退出登录
+// app.post('/logout', function (req, res, next) {
+//     req.session.loginUser = null;
+//     res.clearCookie('testapp');
+//     res.json({success:'退出成功'});
+// });
+
+// // 获取用户列表
+// app.post('/userList', function (req, res, next) {
+//     var login = req.session.loginUser;
+//     var userList = ['ashunadmin'];
+//     var limit =  '';
+//     if (req.body.limit) {
+//         limit = ' where userName = "'+ req.body.limit +'"';
+//     }
+//     var sql = 'SELECT * FROM list'+limit;
+//     if(login && userList.indexOf(login.userName) > -1) {
+//         pool.getConnection(function (err, conn) {
+//             if (err) console.log("POOL userlist-register==> " + err);
+//             conn.query(sql, function (err, result) {
+//                 res.json({list: result});
+//                 conn.release();
+//             });
+//         });
+//     } else {
+//         res.json({list: [], error: '请重新登入'});
+//     }
+// });
+
+// // 更新用户列表
+// app.post('/updateUser', function (req, res, next) {
+//     var login = req.session.loginUser;
+//     var userList = ['ashunadmin'];
+//     var sql = 'update list set endDate ="'+ req.body.endDate +'",total = "'+ req.body.total +'" where userName = "'+ req.body.userName + '"';
+//     if(login && userList.indexOf(login.userName) > -1) {
+//         pool.getConnection(function (err, conn) {
+//             if (err) console.log("POOL userlist-register==> " + err);
+//             conn.query(sql, function (err, result) {
+//                 res.json({success: '更新成功'});
+//                 conn.release();
+//             });
+//         });
+//     } else {
+//         res.json({error: '更新失败'});
+//     }
+// });
--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,58 @@
-var userCont = '<div class="modal my-login"><span class="close" onclick="closeModal()">X</span><h6 class="modal-tite">Wellcome</h6><div class="modal-body"><form action="" id="login-form"><input type="text" name="userName" placeholder="用户名"> <input type="password" name="password" placeholder="密码"></form></div><div class="modal-foot"><button>登&nbsp;&nbsp;&nbsp;录</button><p><font color="red">还没有账号吗？</font><span onclick="showMOdel(&quot;register&quot;)"><font color="#1ab394">去注册</font></span></p></div></div><div class="modal my-register"><span class="close" onclick="closeModal()">X</span><h6 class="modal-tite">Wellcome</h6><div class="modal-body"><form action="" id="register-form"><input type="text" name="userName" placeholder="用户名"> <input type="password" name="password" placeholder="密码"> <input type="password" name="vaidPassword" placeholder="确认密码"></form></div><div class="modal-foot"><button>注&nbsp;&nbsp;&nbsp;册</button><p><font color="red">已有账号吗？</font><span onclick="showMOdel(&quot;login&quot;)"><font color="#1ab394">去登入</font></span></p></div></div>';
+// ajax
+function ajax() {  
+    var ajaxData = {    
+        type: arguments[0].type || "GET",
+            url: arguments[0].url || "",
+            async: arguments[0].async || "true",
+            data: arguments[0].data || null,
+            dataType: arguments[0].dataType || "text",
+            contentType: arguments[0].contentType || "application/x-www-form-urlencoded",
+            beforeSend: arguments[0].beforeSend || function () {},
+            success: arguments[0].success || function () {},
+            error: arguments[0].error || function () {}  
+    }; 
+    ajaxData.beforeSend(); 
+    var xhr = createxmlHttpRequest();
+    try{
+        xhr.responseType = ajaxData.dataType;  
+    }catch (err) {
+        console.log(err)
+    };
+    xhr.open(ajaxData.type, ajaxData.url, ajaxData.async);   
+    xhr.setRequestHeader("Content-Type", ajaxData.contentType);   
+    xhr.send(convertData(ajaxData.data));   
+    xhr.onreadystatechange = function () {     
+        if (xhr.readyState == 4) {       
+            if (xhr.status == 200) {
+                ajaxData.success(xhr.response);      
+            } else {        
+                ajaxData.error();      
+            }     
+        }  
+    } 
+};
+function createxmlHttpRequest() {   
+    if (window.ActiveXObject) {     
+        return new ActiveXObject("Microsoft.XMLHTTP");   
+    } else if (window.XMLHttpRequest) {     
+        return new XMLHttpRequest();   
+    } 
+}; 
+function convertData(data) {  
+    if (typeof data === 'object') {    
+        var convertResult = "";     
+        for (var c in data) {       
+            convertResult += c + "=" + data[c] + "&";     
+        }     
+        convertResult = convertResult.substring(0, convertResult.length - 1);   
+        return convertResult;  
+    } else {    
+        return data;  
+    }
+};
+
+// 内容
+var userCont = '<div class="modal my-login"><span class="close" onclick="closeModal()">X</span><h6 class="modal-tite">Wellcome</h6><div class="modal-body"><form action="" id="login-form"><input type="text" name="userName" placeholder="用户名"> <input type="password" name="password" placeholder="密码"></form></div><div class="modal-foot"><button onclick="getLogin()">登&nbsp;&nbsp;&nbsp;录</button><p><font color="red">还没有账号吗？</font><span onclick="showMOdel(&quot;register&quot;)"><font color="#1ab394">去注册</font></span></p></div></div><div class="modal my-register"><span class="close" onclick="closeModal()">X</span><h6 class="modal-tite">Wellcome</h6><div class="modal-body"><form action="" id="register-form"><input type="text" name="userName" placeholder="用户名"> <input type="password" name="password" placeholder="密码"> <input type="password" name="vaidPassword" placeholder="确认密码"></form></div><div class="modal-foot"><button onclick="getRegister()">注&nbsp;&nbsp;&nbsp;册</button><p><font color="red">已有账号吗？</font><span onclick="showMOdel(&quot;login&quot;)"><font color="#1ab394">去登入</font></span></p></div></div>';
 var mask = document.getElementById('mask');
 // 判断是否为登入状态
 mask.innerHTML = userCont;
@@ -30,6 +84,77 @@ function showMOdel(type) {
     }
     mask.style.display = 'block';
 }
+function getLogin() {
+    var loginForm = document.getElementById('login-form');
+    var userName = loginForm.userName.value;
+    var password = loginForm.password.value;
+    var obj = {userName: userName, password: password };
+    vaidParams(obj, '/login');
+}
+function getRegister() {
+    var registerForm = document.getElementById('register-form');
+    var userName = registerForm.userName.value;
+    var password = registerForm.password.value;
+    var vaidPassword = registerForm.vaidPassword.value;
+    if (vaidPassword !== password) {
+        alert('两次密码输入不一致');
+        return;
+    }
+    var obj = {userName: userName, password: password };
+    vaidParams(obj, '/register');
+}
+function vaidParams(obj, url) {
+    var error = '';
+    for (var key in obj) {
+        if (!obj[key]) {
+            error = '用户或密码不能为空';
+        }
+        if (obj[key].length > 12) {
+            error = '用户或密码不可超过12位';
+        }
+        if (obj[key].length < 3) {
+            error = '用户或密码不可小于3位';
+        }
+    }
+    if (error) {
+        alert(error);
+        return;
+    }
+    ajax({  
+        type: "post",
+        url: url,
+        data: obj,
+        success: function (data) {
+            var result = JSON.parse(data);
+            if (result.error) {
+                alert(result.error);
+            } else {
+                // location.reload();
+                console.log(result)
+            }
+        },
+        error: function () {
+            alert('系统异常，操作失败');
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 公告
 scrollNotice();
 function scrollNotice () {
     var num = 1;
@@ -47,6 +172,6 @@ function scrollNotice () {
                     notice.appendChild(np);
                 }
             }
-        }, 5000)
+        }, 3000)
     }
 }
